2d0ab155498f07cedf1d4ab383fba3774ce877ac
Ben <bsadick@gmail.com>
2017-02-21 21:55:45 +0900

    added some functionality to timer front end

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,22 +1,41 @@
-let seconds = 25 * 60;
+const DEFAULT_SECONDS = 5;
+
+let seconds = DEFAULT_SECONDS;
 let timer;
 let currentTime = document.getElementById("currentTime");
+let isTimerOn = false;
+let pomodoros = 0;
+
 
 function start(){
-  timer = setInterval(function (){
-    seconds--;
-    currentTime.innerHTML = formatTime(seconds);
+  if (isTimerOn) return;
+
+  isTimerOn = true;
+  timer = setInterval(() => {
+    if(seconds === 0){
+       seconds= DEFAULT_SECONDS + 1;
+       updatePomodoro();
+    } else {
+        seconds--;
+        currentTime.innerHTML = formatTime(seconds);
+    }
   }, 1000);
 }
 
 function pause(){
+  isTimerOn = false;
   clearInterval(timer);
 }
 
 function stop(){
-  seconds = 25 * 60;
+  seconds = DEFAULT_SECONDS;
   clearInterval(timer);
   currentTime.innerHTML = formatTime(seconds);
+}
+
+function updatePomodoro(){
+  pomodoros++;
+  document.getElementById("pomodoros").innerHTML = pomodoros;
 }
 
 document.getElementById('startBtn').addEventListener('click', start);
